07383ed70823761f032715d20ff4bed12b8f0fd9
maxx06 <67605840+maxx06@users.noreply.github.com>
2025-08-05 03:47:26 -0400

    working inference

--- mollama/app/(tabs)/index.tsx
+++
@@ -1,5 +0,0 @@
-import ChatScreen from '@/src/components/Chat/ChatScreen';
-
-export default function HomeScreen() {
-  return <ChatScreen />;
-}
--- mollama/app/(tabs)/models.tsx
+++
@@ -1,5 +0,0 @@
-import ModelsScreen from '@/src/components/Models/ModelsScreen';
-
-export default function ModelsTab() {
-  return <ModelsScreen />;
-} 
